122d0221dc39f9944c33520b8ddbe36f22381fc7
Aditta Chakraborty <cseaditta@gmail.com>
2025-08-28 20:47:11 +0600

    call function implemented to remain cards

--- js/index.js
+++ js/index.js
@@ -1,20 +1,43 @@
 const callCost = 20;
 
 // function to call police 2
-function callPolice2(title, number) {
-    const text = document.getElementById(title).innerText;
-    const callingNumber = document.getElementById(number).innerText;
-    alert(`📞 Calling ${text} ${callingNumber}...`);
-}
-
-// function to call railway
-function callRailway(title, number) {
+function call(title, number) {
     const text = document.getElementById(title).innerText;
     const callingNumber = document.getElementById(number).innerText;
     alert(`📞 Calling ${text} ${callingNumber}...`);
 }
 
 // call button access
+document.getElementById("btn-national-emergency-call").addEventListener("click", function(e) {
+    let coinElement = document.getElementById("coin");
+    let coin = parseInt(coinElement.innerText);
+
+    if (coin < callCost) {
+        alert("❌ You don't have enough coin. Minimum 20 coins are required to call.");
+        return;
+    }
+
+    call("national-emergency-title", "national-emergency-number");
+    
+    coin = coin - callCost;
+    coinElement.innerText = coin;
+});
+
+document.getElementById("btn-police1-call").addEventListener("click", function(e) {
+    let coinElement = document.getElementById("coin");
+    let coin = parseInt(coinElement.innerText);
+
+    if (coin < callCost) {
+        alert("❌ You don't have enough coin. Minimum 20 coins are required to call.");
+        return;
+    }
+
+    call("police1-title", "police1-number");
+    
+    coin = coin - callCost;
+    coinElement.innerText = coin;
+});
+
 document.getElementById("btn-police2-call").addEventListener("click", function(e) {
     let coinElement = document.getElementById("coin");
     let coin = parseInt(coinElement.innerText);
@@ -24,7 +47,97 @@ document.getElementById("btn-police2-call").addEventListener("click", function(e
         return;
     }
 
-    callPolice2("police2-title", "police2-number");
+    call("police2-title", "police2-number");
+    
+    coin = coin - callCost;
+    coinElement.innerText = coin;
+});
+
+document.getElementById("btn-fire-service-call").addEventListener("click", function(e) {
+    let coinElement = document.getElementById("coin");
+    let coin = parseInt(coinElement.innerText);
+
+    if (coin < callCost) {
+        alert("❌ You don't have enough coin. Minimum 20 coins are required to call.");
+        return;
+    }
+
+    call("fire-service-title", "fire-service-number");
+    
+    coin = coin - callCost;
+    coinElement.innerText = coin;
+});
+
+document.getElementById("btn-ambulance-call").addEventListener("click", function(e) {
+    let coinElement = document.getElementById("coin");
+    let coin = parseInt(coinElement.innerText);
+
+    if (coin < callCost) {
+        alert("❌ You don't have enough coin. Minimum 20 coins are required to call.");
+        return;
+    }
+
+    call("ambulance-title", "ambulance-number");
+    
+    coin = coin - callCost;
+    coinElement.innerText = coin;
+});
+
+document.getElementById("btn-woman-and-child-call").addEventListener("click", function(e) {
+    let coinElement = document.getElementById("coin");
+    let coin = parseInt(coinElement.innerText);
+
+    if (coin < callCost) {
+        alert("❌ You don't have enough coin. Minimum 20 coins are required to call.");
+        return;
+    }
+
+    call("woman-and-child-title", "woman-and-child-number");
+    
+    coin = coin - callCost;
+    coinElement.innerText = coin;
+});
+
+document.getElementById("btn-anti-corruption-call").addEventListener("click", function(e) {
+    let coinElement = document.getElementById("coin");
+    let coin = parseInt(coinElement.innerText);
+
+    if (coin < callCost) {
+        alert("❌ You don't have enough coin. Minimum 20 coins are required to call.");
+        return;
+    }
+
+    call("anti-corruption-title", "anti-corruption-number");
+    
+    coin = coin - callCost;
+    coinElement.innerText = coin;
+});
+
+document.getElementById("btn-electricity-call").addEventListener("click", function(e) {
+    let coinElement = document.getElementById("coin");
+    let coin = parseInt(coinElement.innerText);
+
+    if (coin < callCost) {
+        alert("You don't have enough coin. Minimum 20 coins are required to call.");
+        return;
+    }
+
+    call("electricity-title", "electricity-number");
+    
+    coin = coin - callCost;
+    coinElement.innerText = coin;
+});
+
+document.getElementById("btn-brac-call").addEventListener("click", function(e) {
+    let coinElement = document.getElementById("coin");
+    let coin = parseInt(coinElement.innerText);
+
+    if (coin < callCost) {
+        alert("You don't have enough coin. Minimum 20 coins are required to call.");
+        return;
+    }
+
+    call("brac-title", "brac-number");
     
     coin = coin - callCost;
     coinElement.innerText = coin;
@@ -39,7 +152,7 @@ document.getElementById("btn-railway-call").addEventListener("click", function(e
         return;
     }
 
-    callRailway("railway-title", "railway-number");
+    call("railway-title", "railway-number");
     
     coin = coin - callCost;
     coinElement.innerText = coin;
